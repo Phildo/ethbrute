781ca1fb77a7e0ef9cab16a7f3d79638720ece70
Phildo <Phildo211@gmail.com>
2017-06-13 20:49:41 -0500

    added ote

--- brute.py
+++ brute.py
@@ -1,15 +1,14 @@
 from multikdf.scrypt import scrypt_kdf
+import hashlib
 import sha3
 
 dklen = 32
 salt = bytearray.fromhex("d77fe79c1162d34e85994a338e815d3904c171c67cc3f72d4b433aa19dbc7bb1")
 r = 8
 p = 1
-n = 18 #262144
+n = 18 #262144 #NOTE- put Log BASE2 n here. so, if n = 262144 in the .json wallet file, put 18 here
 ciphertext = bytearray.fromhex("c6d18f78af6e5e7cea4ab804d8b9a5f974ccf92ea2537416b90abebd77c6b580")
 mac = bytearray.fromhex("89655fd4bb9ccf9de0d8f1a78025a8616c9416ec700cfaafef17e76c7c47338e")
-
-real_password = "thisisatestwallet"
 
 file = open("../password.txt", "r")
 for line in file:
